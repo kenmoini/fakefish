279fb949e35ac234304062aa73594556dc4dacac
Ken Moini <ken@kenmoini.com>
2023-06-30 20:25:30 -0400

    add doc link

--- aadp_scripts/shared_auth.sh
+++ aadp_scripts/shared_auth.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Important docs: https://ftp1.tyan.com/pub/doc/API%20Reference%20Guide_v1.0.pdf
+
 LOGIN=$(curl -skSL --cookie-jar cookies.txt -X POST --form username=${BMC_USERNAME} --form password=${BMC_PASSWORD} -H "Accept: application/json" https://${BMC_ENDPOINT}/api/session)
 if [ $? -ne 0 ]; then
   exit 1
